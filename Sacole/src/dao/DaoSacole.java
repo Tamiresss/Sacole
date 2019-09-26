16393587aad295811794f1831a5c3961778b3ca9
Administrador <Administrador@10.10.4.26>
2019-09-26 16:41:52 -0300

    rqhy

---
+++ Sacole/src/dao/DaoSacole.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Sacole;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author Administrador
+ */
+public class DaoSacole {
+    public static boolean inserir(Sacole objeto) {
+        String sql = "INSERT INTO sacole (sabor, datavalidade, preco, nrserie) VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getSabor());
+            ps.setDate(2, Date.valueOf(objeto.getDatavalidade()));
+            ps.setDouble(3, objeto.getPreco());
+            ps.setInt(4, objeto.getNrserie());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Sacole objeto = new Sacole();
+        objeto.setSabor("Morango");
+        objeto.setDatavalidade(LocalDate.parse("11/01/1988", DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        objeto.setPreco(10.2);
+        objeto.setNrserie(27);
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+    public static boolean alterar(Sacole objeto) {
+        String sql = "UPDATE sacole SET sabor = ?, datavalidade = ?, preco = ?, nrserie = ? WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getSabor()); 
+            ps.setDate(2, Date.valueOf(objeto.getDatavalidade()));
+            ps.setDouble(3, objeto.getPreco());
+            ps.setInt(4, objeto.getNrserie());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+    public static boolean excluir(Sacole objeto) {
+        String sql = "DELETE FROM sacole WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getCodigo());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+    public static List<Sacole> consultar() {
+        List<Sacole> resultados = new ArrayList<>();
+        //editar o SQL conforme a entidade
+        String sql = "SELECT codigo, sabor, datavalidade, preco, nrserie FROM sacole";
+        PreparedStatement ps;
+        try {
+            ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Sacole objeto = new Sacole();
+                //definir um set para cada atributo da entidade, cuidado com o tipo
+                objeto.setCodigo(rs.getInt("codigo"));
+                objeto.setSabor(rs.getString("sabor"));
+                objeto.setDatavalidade(rs.getString("datavalidade"));
+                objeto.setPreco(rs.getDouble("preco"));
+                objeto.setNrserie(rs.getInt("nrserie"));
+ 
+                
+                
+                resultados.add(objeto);//não mexa nesse, ele adiciona o objeto na lista
+            }
+            return resultados;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+}
+    public static Sacole consultar(int primaryKey) {
+        //editar o SQL conforme a entidade
+        String sql = "SELECT codigo, sabor, datavalidade, preco, nrserie FROM sacole WHERE codigo=?";
+        PreparedStatement ps;
+        try {
+            ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, primaryKey);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Sacole objeto = new Sacole();
+                //definir um set para cada atributo da entidade, cuidado com o tipo
+                objeto.setCodigo(rs.getInt("codigo"));
+                objeto.setSabor(rs.getString("sabor"));
+                objeto.setDatavalidade(rs.getString("datavalidade"));
+                objeto.setPreco(rs.getDouble("preco"));
+                objeto.setNrserie(rs.getInt("nrserie"));
+                return objeto;//não mexa nesse, ele adiciona o objeto na lista
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+}
---
+++ Sacole/src/modelo/Sacole.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+import java.time.LocalDate;
+/**
+ *
+ * @author Administrador
+ */
+public class Sacole {
+    private Integer codigo;
+    private String sabor;
+    private Integer nrserie;
+    private Double preco;
+    private LocalDate datavalidade;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public Integer getNrserie() {
+        return nrserie;
+    }
+
+    public void setNrserie(Integer nrserie) {
+        this.nrserie = nrserie;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public LocalDate getDatavalidade() {
+        return datavalidade;
+    }
+
+    public void setDatavalidade(LocalDate datavalidade) {
+        this.datavalidade = datavalidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Sacole{" + "sabor=" + sabor + '}';
+    }
+
+    
+    
+}
